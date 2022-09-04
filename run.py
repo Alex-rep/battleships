a8920163de7a37b2b3c8a7a3e62daa37211b14d4
Alex-rep <nuclearCMP_t1058@protonmail.com>
2022-09-04 13:58:31 +0000

    Add PlayerBoard Object and functions to generate ships and the board itself

--- run.py
+++ run.py
@@ -1,3 +1,49 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class PlayerBoard: 
+
+    player_name = ""
+    score = 0
+    board = []
+    ships = []
+
+    def __init__(self, name):
+        """
+        Constructs the object and inserts the board owner's name
+        """
+
+        self.player_name = name
+    
+    def generate_ships(self):
+        """ 
+        Generates four couples of integers between 0 and 4 that
+        represent the coordinates for the player's ships
+        """
+        for index in range(4):
+
+            ship_row = random.randint(0, 4)
+            ship_column = random.randint(0, 4)
+            ship = [ship_row, ship_column]
+            self.ships.append(ship)
+
+    def generate_board(self):
+        """ 
+        Generates an empty player board
+        """
+
+        column = [".",".",".","."]
+        
+        for index in range(4):
+            self.board.append(column)
+    
+
+
+
+
+player_board = PlayerBoard("player")
+player_board.generate_ships()
+player_board.generate_board()
+print(player_board.ships)
+print(player_board.board)
+
+
