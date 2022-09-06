a9166d78228e2d8ba8ca3054e95c46fd0b4e71f0
Alex-rep <nuclearCMP_t1058@protonmail.com>
2022-09-06 12:15:54 +0000

    Move the generate_ships() function out of the PlayerObject object and modified the constructor to account for a python feature: variables not initialized through the constructor are shared between all objects of the class. Each board should have its own ships.

--- run.py
+++ run.py
@@ -7,24 +7,14 @@ class PlayerBoard:
     board = []
     ships = []
 
-    def __init__(self, name):
+    def __init__(self, name, ship_list):
         """
         Constructs the object and inserts the board owner's name
         """
 
         self.player_name = name
+        self.ships = ship_list
     
-    def generate_ships(self):
-        """ 
-        Generates four couples of integers between 0 and 4 that
-        represent the coordinates for the player's ships
-        """
-        for index in range(4):
-
-            ship_row = index
-            ship_column = random.randint(0, 3)
-            ship = [ship_row, ship_column]
-            self.ships.append(ship)
 
     def generate_board(self):
         """ 
@@ -46,6 +36,22 @@ class PlayerBoard:
             column = self.ships[index][1]
             self.board[row][column] = "@"
            
+def generate_ships():
+    """ 
+    Generates four couples of integers between 0 and 4 that
+    represent the coordinates for the player's ships
+    """
+    ship_list = []
+
+    for index in range(4):
+
+        ship_row = index
+        ship_column = random.randint(0, 3)
+        ship = [ship_row, ship_column]
+        ship_list.append(ship)
+    
+    return ship_list
+
 def roll_guess(): 
     """ 
     Generates a couple of integers, guesses
@@ -66,7 +72,7 @@ def get_guess():
     print("Guess a column: \n")
     column = input()
     
-    result = [row, column]
+    result = [int(row) - 1 , int(column) - 1]
     return result
 
 def print_board(player_board):
@@ -74,18 +80,37 @@ def print_board(player_board):
     Prints the board for the specified player
     """
     for index in range(4):
-    print(player_board.board[index])
-    print("\n")
+        print(player_board.board[index])
+        print("\n")
 
-player_board = PlayerBoard("player")
-player_board.generate_ships()
+player_board = PlayerBoard("player", generate_ships())
 player_board.generate_board()
 
-cpu_board = PlayerBoard("CPU")
-cpu_board.generate_ships()
+cpu_board = PlayerBoard("CPU", generate_ships())
 cpu_board.generate_board()
 
 player_board.add_ships()
+cpu_board.add_ships()
+
+print(player_board.ships)
+print(cpu_board.ships)
+
+#for index in range(4):
+
+    #print("Player's board: \n")
+    #print_board(player_board)
+    #print("CPU's board: \n")
+    #print_board(cpu_board)
+    #print(cpu_board.ships)
+
+    #guess = get_guess()
+
+    #for index in range(4):
+        
+        #if guess[0] == cpu_board.ships[index][0] and guess[1] == cpu_board.ships[index][1]:
+         #   print("It's a hit! \n")
+          #  player_board.score += 1
+           # break
 
 
 
